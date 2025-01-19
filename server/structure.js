9c2530aae12a12ff46a93ad86fde9b57d39fa090
Samarth Verma <verma.samarth05@gmail.com>
2025-01-18 21:45:22 -0500

    defined stricter functions

--- server/structure.js
+++ server/structure.js
@@ -1,11 +1,11 @@
-const descriptionHandler = () => {
-
+const descriptionHandler = (description) => {
+    // use description to generate a score from 1 - 5
 }
 
-const manufacturingHandler = () => {
-
+const manufacturingHandler = (product) => {
+    // use product to generate a score from 1 - 5
 }
 
-const supplyChainHandler = () => {
-    
+const supplyChainHandler = (company) => {
+    // use company name to generate a score from 1 - 5
 }
